6942e03e26bad6b4dbebc19e23e3ce33c863dbf0
Fabrizzio Jiglau <148718617+fabeezz@users.noreply.github.com>
2023-12-18 01:26:33 +0200

    Delete L3 directory

--- L3/Op. cu LISTE/pb1.py
+++
@@ -1,6 +0,0 @@
-n=int(input("Lungime liste:")) #[1,2,3,4,5]
-l1=[int(x) for x in input("Lista1:").split()]
-l2=[int(x) for x in input("Lista2:").split()]
-for i in range(0,n,2):
-    if i%2==0:
-        l1[i:i]
